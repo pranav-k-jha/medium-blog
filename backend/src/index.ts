7274c6c12948d9ca059ab31127d1c069ee4161ee
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-05-28 20:52:01 -0400

    updated sign up route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,29 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.post("/api/v1/signup", (c) => {
+app.post("/api/v1/signup", async (c) => {
   const prisma = new PrismaClient({
-    datasourceUrl: env.DATABASE_URL,
+    datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
   return c.text("Sign Up");
 });
 
